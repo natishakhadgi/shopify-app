77b5d9a584fb235b98357671ad0516aee59e8d4a
natishakhadgi <natisha.khadgi@sjsu.edu>
2021-09-19 11:01:01 -0700

    add imagesLoaded check for masonry

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useLayoutEffect, useState} from "react";
 import Header from "./Header";
 import Blurb from "./Blurb";
 import Post from "./Post";
 import Footer from "./Footer";
 
 import Masonry from "masonry-layout";
+import imagesLoaded from "imagesloaded";
 
 /*
 Color Scheme: https://www.colorcombos.com/color-schemes/275/ColorCombo275.html
@@ -12,14 +13,15 @@ https://docs.google.com/document/d/1QlC6htA5SXEl3YruAOkJWj2-0W3w-n0UOzGuJ1EcktQ/
 
 TO DO:
 Place Loading
+onClick: link copied only!
 */
 
-// Use Masonry to create Masonry/Mosaic Effect for Gallery
 function App() {
 
 const [isLoading, setLoading] = useState(true);
 const [posts, setPosts] = useState(null);
 const [isError, setError] = useState(false);
+const [areImagesLoaded, setImagesLoaded] = useState(false);
 
 let start = new Date();
 start.setDate(start.getDate() - 20);
@@ -51,8 +53,11 @@ useEffect(() => {
 useEffect(() => {
   if(posts){
     setLoading(false);
-    const gallery = document.querySelector('.wrapper');
-    const masonry = new Masonry(gallery, {gutter: 20});
+    imagesLoaded( document.querySelector('.wrapper'), function() {
+      console.log('all images are loaded');
+      const gallery = document.querySelector('.wrapper');
+      const masonry = new Masonry(gallery, {gutter: 20});
+    });  
   }
 })
 
